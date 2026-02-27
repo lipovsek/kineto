854891f776dc516eb31c9dd26081a9e30f15d96c
generatedunixname1587093422349604 <noreply+1587093422349604@fb.com>
2026-02-27 13:04:51 -0800

    Fix CQS signal modernize-use-designated-initializers in fbcode/kineto/libkineto
    Reviewed By: fenypatel99
    
    Differential Revision: D94492651
    
    fbshipit-source-id: ab89d2a8e662173db25d81f02c7c58051cec0ae9

--- libkineto/src/CuptiCbidRegistry.cpp
+++ libkineto/src/CuptiCbidRegistry.cpp
@@ -34,8 +34,10 @@ void CuptiCbidRegistry::registerCallback(
     uint32_t cbid,
     bool requiresFlowCorrelation,
     bool isBlocklisted) {
-  getMapForDomain(domain)[cbid] =
-      CbidProperties{requiresFlowCorrelation, isBlocklisted, {}};
+  getMapForDomain(domain)[cbid] = CbidProperties{
+      .requiresFlowCorrelation = requiresFlowCorrelation,
+      .isBlocklisted = isBlocklisted,
+      .name = {}};
 }
 
 void CuptiCbidRegistry::registerCallback(
@@ -44,8 +46,10 @@ void CuptiCbidRegistry::registerCallback(
     bool requiresFlowCorrelation,
     bool isBlocklisted,
     const std::string& name) {
-  getMapForDomain(domain)[cbid] =
-      CbidProperties{requiresFlowCorrelation, isBlocklisted, name};
+  getMapForDomain(domain)[cbid] = CbidProperties{
+      .requiresFlowCorrelation = requiresFlowCorrelation,
+      .isBlocklisted = isBlocklisted,
+      .name = name};
 }
 
 void CuptiCbidRegistry::registerCallbackRange(
@@ -56,7 +60,11 @@ void CuptiCbidRegistry::registerCallbackRange(
     bool isBlocklisted) {
   cbidRanges_.push_back(
       {domain,
-       CbidRange{startCbid, endCbid, requiresFlowCorrelation, isBlocklisted}});
+       CbidRange{
+           .startCbid = startCbid,
+           .endCbid = endCbid,
+           .requiresFlowCorrelation = requiresFlowCorrelation,
+           .isBlocklisted = isBlocklisted}});
 }
 
 CuptiCbidRegistry::CuptiCbidRegistry() {
